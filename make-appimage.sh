e69aa3ec0d6648da8617608bd6d08dd99f622f89
Samueru-sama <xdglawyer@outlook.com>
2026-03-20 22:20:13 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,22 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q bulky | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/bulky.svg
+export DESKTOP=/usr/share/applications/bulky.desktop
+export DEPLOY_PYHON=1
+export ALWAYS_SOFTWARE=1
+
+# /usr/bin/bulky is a bash script that just executes bulky
+# there is no point in that, just symlink the python script directly lol
+ln -sf /usr/lib/bulky/bulky.py /usr/bin/bulky
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/bulky /usr/lib/libgtk-3.so*
 
 # Additional changes can be done in between here
 
